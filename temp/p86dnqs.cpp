00697a32cf182dfbad067f86c96c96540513f4f7
mounika69 <vadlamounika0609@gmail.com>
2024-05-14 11:36:14 +0530

    First Commit

---
+++ temp/p86dnqs.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int first_number, second_number, sum;
+    
+ 
+  cin >> first_number >> second_number;
+
+  // sum of two numbers in stored in variable sumOfTwoNumbers
+  sum = first_number + second_number;
+
+  cout<<"Sum of two numbers : ";
+  // prints sum 
+  cout << first_number << " + " <<  second_number << " = " << sum;     
+
+  return 0;
+}
